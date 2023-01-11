d8c8e725ed57d39828b1b68c669af6ad1d2db4f5
Isabella <belladougherty@gmail.com>
2023-01-11 08:46:25 -0700

    Overall project first upload

---
+++ HelloWorldAdjusted/HelloWorldAdjusted/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim message As String = ""
+        Console.WriteLine("Hello World!")
+        message = Console.ReadLine()
+        Console.WriteLine($"You said: {message}!")
+        Console.WriteLine("Have a nice day!!!")
+    End Sub
+End Module
